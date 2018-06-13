28940a187bae35b16ff7f4f300c171f82d370c6c
Narathip <colkeover@gmail.com>
2018-06-13 16:31:36 +0700

    Add "Login Page" and "Serial Login" activities

--- app/src/main/java/com/example/onewdivideslaptop/shareholder_application/LoginPage.java
+++ app/src/main/java/com/example/onewdivideslaptop/shareholder_application/LoginPage.java
@@ -2,6 +2,9 @@ package com.example.onewdivideslaptop.shareholder_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        ((Button)findViewById(R.id.qr_login_btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start <QR camera> activity
+            }
+        });
+
+        ((Button)findViewById(R.id.qr_login_icon_btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start <QR camera> activity
+            }
+        });
+
+        ((Button)findViewById(R.id.serial_login_btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start <Username log-in> activity
+            }
+        });
+
+        ((Button)findViewById(R.id.serial_login_icon_btn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start <Username log-in> activity
+            }
+        });
     }
 }
