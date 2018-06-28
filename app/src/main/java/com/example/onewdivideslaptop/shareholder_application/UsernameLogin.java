e4ffc79c5eb03e85a2666c647857d4a42be095e4
bankuoniichan <colkeover@gmail.com>
2018-06-28 10:23:28 +0700

    Add missing codes

--- app/src/main/java/com/example/onewdivideslaptop/shareholder_application/UsernameLogin.java
+++ app/src/main/java/com/example/onewdivideslaptop/shareholder_application/UsernameLogin.java
@@ -14,13 +14,15 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 public class UsernameLogin extends AppCompatActivity {
 
     ImageButton backBtn;
     Button loginBtn;
-
     Dialog dialog;
     EditText usernameInput;
+    Pattern delegateId_pattern = Pattern.compile("[0-9]+");
 
 
     @Override
@@ -49,62 +51,64 @@ public class UsernameLogin extends AppCompatActivity {
             }
         });
 
+    }
 
+    private boolean isValidId(String delegateId){
+        return delegateId_pattern.matcher(delegateId).matches();
+    }
+
+    public void showConfirmPopup(View v){
+        String delegateId = usernameInput.getText().toString();
+        if(delegateId.length()==0){
+            Toast.makeText(this,"กรุณาใส่ Serial Number",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(!isValidId(delegateId)){
+            Toast.makeText(this,"กรุณาใส่เพียงตัวเลข",Toast.LENGTH_SHORT).show();
+            return;
         }
 
-        public void showConfirmPopup(View v){
-            String delegateId = usernameInput.getText().toString();
-            if(delegateId.length()==0){
-                Toast.makeText(this,"กรุณาใส่ Serial Number",Toast.LENGTH_SHORT).show();
-                return;
+        AppUtility.login(delegateId, new Runnable() {
+            @Override
+            public void run() {
+                dialog.setContentView(R.layout.login_popup);
+                TextView txtClose = (TextView) dialog.findViewById(R.id.txtClose);
+                Button yesBtn = (Button) dialog.findViewById(R.id.yesBtn);
+                Button noBtn = (Button) dialog.findViewById(R.id.noBtn);
+
+                TextView usernameTextView = dialog.findViewById(R.id.Username);
+                usernameTextView.setText(AppUtility.getDelegateThaiName()+"\n"+AppUtility.getDelegateEngName());
+
+                yesBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(UsernameLogin.this, MainPage.class);
+                        startActivity(intent);
+                    }
+                });
+
+                noBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                Log.e("popUp", "Show");
+                txtClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.e("popUp", "Close");
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
             }
-            if(!isValidId(delegateId)){
-                Toast.makeText(this,"กรุณาใส่เพียงตัวเลข",Toast.LENGTH_SHORT).show();
-                return;
-            }
+        });
 
-            AppUtility.login(delegateId, new Runnable() {
-                @Override
-                public void run() {
-                    dialog.setContentView(R.layout.login_popup);
-                    TextView txtClose = (TextView) dialog.findViewById(R.id.txtClose);
-                    Button yesBtn = (Button) dialog.findViewById(R.id.yesBtn);
-                    Button noBtn = (Button) dialog.findViewById(R.id.noBtn);
-
-                    TextView usernameTextView = dialog.findViewById(R.id.Username);
-                    usernameTextView.setText(AppUtility.getDelegateThaiName()+"\n"+AppUtility.getDelegateEngName());
-
-                    yesBtn.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            Intent intent = new Intent(UsernameLogin.this, MainPage.class);
-                            startActivity(intent);
-                        }
-                    });
-
-                    noBtn.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            dialog.dismiss();
-                        }
-                    });
-
-                    Log.e("popUp", "Show");
-                    txtClose.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            Log.e("popUp", "Close");
-                            dialog.dismiss();
-                        }
-                    });
-
-                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                    dialog.show();
-                }
-            });
-
-        }
+    }
 
 }
-
 
--- app/src/main/java/com/example/onewdivideslaptop/shareholder_application/ViewAgendaItem.java
+++ app/src/main/java/com/example/onewdivideslaptop/shareholder_application/ViewAgendaItem.java
@@ -1,6 +1,10 @@
 package com.example.onewdivideslaptop.shareholder_application;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
