b65a3df3adde0fd0df932210093e775a7e2750e6
bankuoniichan <colkeover@gmail.com>
2018-06-28 13:44:23 +0700

    Add token auth

--- app/src/main/java/com/example/onewdivideslaptop/shareholder_application/shareHolderClient.java
+++ app/src/main/java/com/example/onewdivideslaptop/shareholder_application/shareHolderClient.java
@@ -7,6 +7,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.PATCH;
 import retrofit2.http.Path;
 
@@ -16,20 +17,20 @@ public interface shareHolderClient {
     Call<List<getNameResponse>> getName(@Path("delegate_Id") String delegate_Id);
 
     @GET("/checkAuthorityForVoteAll/{delegate_Id}")
-    Call<List<checkAuthorityForVoteAllResponse>> checkAuthorityForVoteAll(@Path("delegate_Id") String delegate_id);
+    Call<List<checkAuthorityForVoteAllResponse>> checkAuthorityForVoteAll(@Header("Authorization") String token, @Path("delegate_Id") String delegate_id);
 
     @PATCH("voteAll/{voteChoice}")
-    Call<String> voteAll(@Path("voteChoice") String voteChoice, @Body List<voteAllResponse> voteAllBody);
+    Call<String> voteAll(@Header("Authorization") String token, @Path("voteChoice") String voteChoice, @Body List<voteAllResponse> voteAllBody);
 
     @GET("/agendaForClient")
-    Call<List<agendaForClientResponse>> getAllagenda();
+    Call<List<agendaForClientResponse>> getAllagenda(@Header("Authorization") String token);
 
     @GET("/agendaForClient/{agendaId}")
-    Call<List<agendaForClientResponse>> getAgenda(@Path("agendaId") String agendaId);
+    Call<List<agendaForClientResponse>> getAgenda(@Header("Authorization") String token, @Path("agendaId") String agendaId);
 
     @GET("/checkAuthorityForVote/{agendaId}/{delegate_ID}")
-    Call<List<checkAuthorityForVoteAgendaResponse>> checkAuthorityForVote(@Path("agendaId")int agendaId, @Path("delegate_ID") String delegate_ID);
+    Call<List<checkAuthorityForVoteAgendaResponse>> checkAuthorityForVote(@Header("Authorization") String token, @Path("agendaId")int agendaId, @Path("delegate_ID") String delegate_ID);
 
     @PATCH("/vote/{voteChoice}/{agenda_Id}")
-    Call<String> vote(@Path("voteChoice") String voteChoice, @Path("agenda_Id") int agenda_Id, @Body List<voteResponse> voteBody);
+    Call<String> vote(@Header("Authorization") String token, @Path("voteChoice") String voteChoice, @Path("agenda_Id") int agenda_Id, @Body List<voteResponse> voteBody);
 }
